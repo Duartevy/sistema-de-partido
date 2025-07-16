a129c755bb94f819ad19d429b3c47a2af252cc6f
Duartevy <milenegba@gmail.com>
2025-07-16 06:44:49 -0300

    Revertendo a rota inicial

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Web\PartidoWebController;
 use App\Http\Controllers\Web\VereadorWebController;
 
 // Página inicial com lista de partidos
-Route::get('/', [VereadorWebController::class, 'index'])->name('home'); // 
-Route::get('/vereadores', [VereadorWebController::class, 'index'])->name('vereador.index');
+Route::get('/', [PartidoWebController::class, 'index'])->name('home'); // 
+Route::get('/partidos', [PartidoWebController::class, 'index'])->name('partidos.index');
 
 // Rotas de partidos (formulários e ações visuais)
 Route::resource('partidos', PartidoWebController::class)->except(['index']);
